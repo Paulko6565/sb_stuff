fb174160612458a0c2db749d45d939fd87144863
Admin <paul.poyda@gmail.com>
2024-02-22 18:09:21 +0530

    123

--- AltController.py
+++ AltController.py
@@ -4,16 +4,21 @@ import pygetwindow as gw
 import pyautogui
 from time import sleep
 
+pyautogui.useImageNotFoundException()
+
 whole_screen = pyautogui.screenshot()
 
 width = whole_screen.width
 height = whole_screen.height
 
+def scroll(direction):
+    pyautogui.scroll(direction)
+
 def pressKey(key):
     keyboard.press(key)
-    sleep(0.02)
+    sleep(0.015)
     keyboard.release(key)
-    sleep(0.02)
+    sleep(0.015)
 
 def holdKey(key, duration):
     pydirectinput.keyDown(key)
@@ -47,7 +52,25 @@ def reset():
     pressKey("r")
     pressKey("enter")
 
+def useAbility():
+    gloveArea = pyautogui.screenshot(region=(925, 1000, 100, 100))
+    gloveEquipped = scanForColor(gloveArea, 90, 142, 233)
+
+    if gloveEquipped:
+        print("the user has their glove equipped")
+        pressKey("e")
+    else:
+        pressKey("1")
+        pressKey("e")
+
 def dance():
+    gloveArea = pyautogui.screenshot(region=(925, 1000, 100, 100))
+    gloveEquipped = scanForColor(gloveArea, 90, 142, 233)
+
+    if gloveEquipped:
+        print("the user has their glove equipped")
+        pressKey("1")
+
     pressKey("/")
     chat("e dance")
 
@@ -56,7 +79,9 @@ def changeCameraAngle():
         return
 
     holdKey("i", 1)
-    holdKey("o", 0.3)
+    for i in range(1, 7):
+        scroll(-150)
+        sleep(0.1)
 
     if pyautogui.pixel(990, 115)[0] < 200:
         pydirectinput.mouseDown(0, 0, "right")
@@ -91,9 +116,7 @@ def walkToArena():
         pressKey("space")
         holdKey("w", 1)
 
-    elif pyautogui.pixelMatchesColor(mouse_pos[0], mouse_pos[1], (30, 10, 50),
-                                     tolerance=80) or pyautogui.pixelMatchesColor(mouse_pos[0], mouse_pos[1],
-                                                                                  (130, 130, 130), tolerance=99) or \
+    elif pyautogui.pixelMatchesColor(mouse_pos[0], mouse_pos[1], (30, 10, 50), tolerance=80) or pyautogui.pixelMatchesColor(mouse_pos[0], mouse_pos[1],(130, 130, 130), tolerance=99) or \
             pyautogui.pixel(mouse_pos[0], mouse_pos[1])[0] <= 250:
         print("the portal is a little away from behind the player")
         pressKey("space")
@@ -106,6 +129,55 @@ def walkToArena():
         reset()
 
 
+def checkChat():
+    chatMessage = pyautogui.screenshot(region=(0, 254, 400, 25))
+    chatMessage.save(r"C:\Users\Admin\PycharmProjects\pythonProject\chat.png")
+
+    #reset command
+    try:
+        resetCommand = pyautogui.locateOnScreen("reset.png", grayscale=True, confidence=0.95, region=(0, 274, 400, 25))
+        if resetCommand:
+            chat("...")
+            action("reset")
+    except pyautogui.ImageNotFoundException:
+        pass
+
+    #arena command
+    try:
+        arenaCommand = pyautogui.locateOnScreen("arena.png", grayscale=True, confidence=0.95, region=(0, 274, 400, 25))
+        if arenaCommand:
+            chat("...")
+            action("camera")
+            action("arena")
+    except pyautogui.ImageNotFoundException:
+        pass
+
+    #dance command
+    try:
+        danceCommand = pyautogui.locateOnScreen("dance.png", grayscale=True, confidence=0.95, region=(0, 274, 400, 25))
+        if danceCommand:
+            chat("...")
+            action("dance")
+    except pyautogui.ImageNotFoundException:
+        pass
+
+    #jump command
+    try:
+        jumpCommand = pyautogui.locateOnScreen("jump.png", grayscale=True, confidence=0.95, region=(0, 274, 400, 25))
+        if jumpCommand:
+            chat("...")
+            action("jump")
+    except pyautogui.ImageNotFoundException:
+        pass
+
+    #ability command
+    try:
+        abilityCommand = pyautogui.locateOnScreen("ability.png", grayscale=True, confidence=0.95, region=(0, 274, 400, 25))
+        if abilityCommand:
+            chat("...")
+            action("ability")
+    except pyautogui.ImageNotFoundException:
+        pass
 def action(action, argument=None):
     # Get all windows
     windows = gw.getAllWindows()
@@ -136,6 +208,8 @@ def action(action, argument=None):
                 sleep(0.05)
                 window.close()
                 sleep(0.05)
+            elif action == "ability":
+                useAbility()
             else:
                 print("Error")
                 return
@@ -145,8 +219,9 @@ def action(action, argument=None):
     if previous_focused_window:
         previous_focused_window.activate()
 
-action("reset")
 
-sleep(5)
+sleep(1)
 
-action("disconnect")
+while True:
+    checkChat()
+    sleep(1)
