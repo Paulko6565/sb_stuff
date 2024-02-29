4d81c321108927f285e4abd293e5d7d7075ca928
Admin <paul.poyda@gmail.com>
2024-02-29 06:41:39 +0530

    NOT FINISHED, DO NOT USE

--- ScytheBot/ScytheBot.py
+++ ScytheBot/ScytheBot.py
@@ -80,114 +80,109 @@ while True:
         sleep(1)
         pass
     else:
-        move(500, 500)
-
-        #move
-        hold_key("a", 1)
-        hold_key("w", 1)
-
-        #place a scarecrow
-        place_troop(1, 731, 521)
-
-        #place a farmer1 next to the scarecrow
-        place_troop(2, 751, 521)
-
-        close()
-
-        #set the farmer's priority to first
-        click(751, 521)
-        priority("First")
-
-
-        #move
-        hold_key("a", 1)
-
-        #wait for money
-        sleep(6)
-
-        #place a scarecrow
-        place_troop(1, 526, 631)
-
-        #wait for money
-        sleep(7)
-
-        #i think you get the drill by now
-        place_troop(2, 636, 631)
-
-        close()
-
-        click(636, 631)
-
-        priority("First")
-
-        sleep(6)
-
-        click(636, 631)
-
-        buy_upgrade()
-
-        sleep(1)
-
-        click(526, 631)
-
-        buy_upgrade()
-
-        close()
-
-        hold_key("d", 1)
-
-        sleep(5)
-
-        click(731, 521)
-
-        buy_upgrade()
-
-        close()
-
-        hold_key("s", 1)
-
-        sleep(24)
-
-        place_troop(3, 1264, 537)
-
-        click(1264, 537)
-        priority("First")
-
-        sleep(16)
-
-        place_troop(3, 1349, 531)
-
-        click(1349, 531)
-        priority("First")
-
-        hold_key("s", 1)
-
-        sleep(9)
-
-        place_troop(3, 1312, 574)
-
-        click(1312, 574)
-        priority("Last")
-
-        sleep(11)
-
-        place_troop(3, 1373, 570)
-
-        click(1373, 570)
-        priority("Last")
-
-        close()
-
-        hold_key("w", 1)
-
-        sleep(14)
-
-        click(1264, 537)
-        buy_upgrade()
-
-        sleep(10)
-
-        click(1349, 531)
-        buy_upgrade()
-
         break
+
+
+move(500, 500)
+
+#move
+hold_key("a", 1)
+hold_key("w", 1)
+
+#place a scarecrow
+place_troop(1, 731, 521)
+
+#place a farmer1 next to the scarecrow
+place_troop(2, 751, 521)
+
+close()
+
+#set the farmer's priority to first
+click(751, 521)
+priority("First")
+
+
+#move
+hold_key("a", 1)
+
+#wait for money
+sleep(6)
+
+#place a scarecrow
+place_troop(1, 526, 631)
+
+#wait for money
+sleep(7)
+
+#i think you get the drill by now
+place_troop(2, 636, 631)
+
+close()
+
+click(636, 631)
+priority("First")
+
+sleep(6)
+
+click(636, 631)
+buy_upgrade()
+
+sleep(1)
+
+click(526, 631)
+buy_upgrade()
+close()
+
+hold_key("d", 1)
+
+sleep(5)
+
+click(731, 521)
+buy_upgrade()
+close()
+
+hold_key("s", 1)
+
+sleep(24)
+
+place_troop(3, 1264, 537)
+
+click(1264, 537)
+priority("First")
+
+sleep(16)
+
+place_troop(3, 1349, 531)
+
+click(1349, 531)
+priority("First")
+
+hold_key("s", 1)
+
+sleep(9)
+
+place_troop(3, 1312, 574)
+
+click(1312, 574)
+priority("Last")
+
+sleep(11)
+
+place_troop(3, 1373, 570)
+
+click(1373, 570)
+priority("Last")
+
+hold_key("w", 1)
+
+sleep(14)
+
+click(1264, 537)
+buy_upgrade()
+close()
+
+sleep(10)
+
+click(1349, 531)
+buy_upgrade()
+close()
